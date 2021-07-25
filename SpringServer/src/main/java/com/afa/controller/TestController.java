11b16be5f6195d605a4a00e3c0fed0d93589dbc9
BPat123 <bp50@st-andrews.ac.uk>
2021-07-25 19:13:26 +0100

    All tests passing commit before changing testing db

--- SpringServer/src/main/java/com/afa/controller/TestController.java
+++ SpringServer/src/main/java/com/afa/controller/TestController.java
@@ -3,11 +3,15 @@ package com.afa.controller;
 import com.afa.model.UserEntity;
 import com.afa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.afa.model.CourseEntity;
 import com.afa.repositories.CourseRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -44,15 +48,14 @@ public class TestController {
     }
 
     @GetMapping("/findallusers")
-    public String findAllUsers(){
+    public List<UserEntity> findAllUsers(){
+        return userRepository.findAll();
+    }
 
-        String result = "";
-
-        for(UserEntity user : userRepository.findAll()){
-            result += user.toString() + "";
-        }
-
-        return result;
+    @GetMapping("/finduser/{email}")
+    public ResponseEntity findUser(@PathVariable String email){
+        UserEntity user = userRepository.findByEmail(email);
+        return ResponseEntity.ok().body(user);
     }
 }
 
